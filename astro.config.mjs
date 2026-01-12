fcc097744b1619120c6c22487ab37de8e5039784
Maria Robinson <maria.robinson@storyblok.com>
2026-01-12 14:14:39 -0800

    astro ultimate guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,39 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import { storyblok } from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import mkcert from 'vite-plugin-mkcert'
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+const { STORYBLOK_DELIVERY_API_TOKEN } = loadEnv(
+  import.meta.env.MODE,
+  process.cwd(),
+  '',
+);
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_DELIVERY_API_TOKEN,
+      // Only required when using the live preview functionality:
+      livePreview: true,
+      bridge: {
+        resolveRelations: ['featured-articles.articles'],
+      },
+      apiOptions: {
+        region: 'eu',
+      },
+      components: {
+        page: "storyblok/Page",
+        grid: "storyblok/Grid",
+        feature: "storyblok/Feature",
+        teaser: "storyblok/Teaser",
+        ['article-overview']: 'storyblok/ArticleOverview',
+        article: 'storyblok/Article',
+        ['featured-articles']: 'storyblok/FeaturedArticles',
+      },
+    }),
+  ],
+  output: 'server',
+  vite: {
+    plugins: [ mkcert() ]
+  },
+});
